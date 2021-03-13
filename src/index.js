2c0f3c2c2892f7083d6f542d188c0b7ed8f96d20
nbcjoke <79941934+nbcjoke@users.noreply.github.com>
2021-03-13 16:35:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 module.exports = function toReadable (number) {
-    let a;
-    const On = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    const Tn = ['', '', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-    const Hundreds = ['', 'one hundred','two hundred','three hundred','four hundred','five hundred','six hundred','seven hundred','eight hundred','nine hundred'];
-    if (number > 119 && number < 1000) {
-       if(number % 100 < 20) {
-          a = Hundreds[Math.floor(number / 100)] + '' + On[number % 100]
-       } else {
-              a = Hundreds[Math.floor(number / 100)] + '' + Tn[Math.floor((number / 100) / 10)] + '' + On[number % 10];
-         }
-        return a.trim();
-    }
-    if (number > 99 && number < 120) {
-       a = Hundreds[Math.floor(number / 100)] + '' + On[number % 100];
-       return a.trim();
-    }
-    if (number >= 20 && number < 100) {
-       a = Tn[Math.floor(number / 10)] + '' + On[number % 10];
-       return a.trim();
-    }
-    if (number < 20) {
-       if(number === 0) {
-          return 'zero';
-       }
-       return On[number]; 
-    }
+ let a ;
+ const fromOneToTwenty = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+ const dozens = ['',  '',  'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+ const hundreds = ['', 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred']
+if (number < 20 ) {
+ if (number === 0) {
+   return 'zero';
+ }
+   return fromOneToTwenty[number];
+}
+if (number<100 && number>=20) {
+ s = dozens[Math.floor(number / 10)] +' '+ fromOneToTwenty[number%10]
+   return s.trim();
+}
+if (number > 99 && number < 120){
+ s = hundreds[Math.floor(number / 100)] +' '+ fromOneToTwenty[number % 100];
+   return s.trim();
+}
+if (number > 119 && number < 1000) {
+ if (number % 100 < 20) {
+ s = hundreds[Math.floor(number / 100)] +' '+ fromOneToTwenty[number % 100]
+ } else {
+ s = hundreds[Math.floor(number / 100)] +' '+ dozens[Math.floor((number % 100) / 10)] +' '+ fromOneToTwenty[number % 10];
+ }
+   return s.trim();
+ }
 }
