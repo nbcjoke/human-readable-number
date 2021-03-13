9ae4a30f9c161b8ffb770b21e32ec0967ba234b9
nbcjoke <79941934+nbcjoke@users.noreply.github.com>
2021-03-13 16:29:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function toReadable (number) {
     let a;
-    const On = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    const Tn = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-    const Hundreds = ['one hundred','two hundred','three hundred','four hundred','five hundred','six hundred','seven hundred','eight hundred','nine hundred'];
+    const On = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    const Tn = ['', '', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    const Hundreds = ['', 'one hundred','two hundred','three hundred','four hundred','five hundred','six hundred','seven hundred','eight hundred','nine hundred'];
     if (number > 119 && number < 1000) {
        if(number % 100 < 20) {
           a = Hundreds[Math.floor(number / 100)] + '' + On[number % 100]
